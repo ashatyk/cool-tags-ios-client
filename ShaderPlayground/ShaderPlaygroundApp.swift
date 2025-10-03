bc403e204ff934994f3a622ec91b752463857e46
kononov.ruslan <kononov.ruslan@wb.ru>
2025-10-03 21:15:08 +0300

    Initial Commit

---
+++ ShaderPlayground/ShaderPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaderPlaygroundApp.swift
+//  ShaderPlayground
+//
+//  Created by ASHATYK on 03.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct ShaderPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
